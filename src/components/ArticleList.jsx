3da2e54682476629688adedf5593932f7da43380
Zahed Miah <zahedm70@gmail.com>
2023-03-14 11:02:38 +0000

    added loading to articleList

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -3,25 +3,33 @@ import { getArticles } from "../api";
 
 const ArticleList = () => {
   const [articles, setArticles] = useState([]);
+  const [isLoading, SetIsLoading] = useState(true)
 
   useEffect(() => {
     getArticles()
       .then((data) => {
         setArticles(data);
+        SetIsLoading(false)
       });
   }, []);
 
   return (
-    <ul>
-      {articles.map((article) => (
-        <li key={article.article_id}>
-          <p>{article.title}:</p>
-          <p>{article.author}</p>
-          <p>{article.created_at}:</p>
-        </li>
-      ))}
-    </ul>
-  );
-};
+    <div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {articles.map((article) => (
+            <li key={article.article_id}>
+              <p>{article.title}:</p>
+              <p>{article.author}</p>
+              <p>{article.created_at}:</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  )}
+  
 
-export default ArticleList;
+export default ArticleList
