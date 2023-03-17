f18071b5d2d7f7277b9084c66a47fc82796fec36
Zahed Miah <zahedm70@gmail.com>
2023-03-17 10:08:38 +0000

    makde requested changes

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -18,14 +18,23 @@ const Article = () => {
     });
   }, [article_id]);
 
-
   const onClick = () => {
+    const hasVoted = localStorage.getItem(`voted-${article.article_id}`);
+    if (hasVoted) {
+      setIsVotingErr(true);
+      return;
+    }
+
     setIsVotingErr(false);
     setUserVote(1);
-    voteForArticle(article.article_id).catch(() => {
-      setUserVote(0);
-      setIsVotingErr(true);
-    });
+    voteForArticle(article.article_id)
+      .then(() => {
+        localStorage.setItem(`voted-${article.article_id}`, true);
+      })
+      .catch(() => {
+        setUserVote(0);
+        setIsVotingErr(true);
+      });
   };
 
   return (
@@ -43,9 +52,7 @@ const Article = () => {
               <img src={article.article_img_url} alt="Article" />
               <p>{article.body}</p>
               <p>Votes: {article.votes + userVote}</p>
-              <button onClick={onClick}>
-                Vote
-              </button>
+              <button onClick={onClick}>Vote</button>
               {isVotingErr && <p>Unable to vote!</p>}
             </li>
           </ul>
